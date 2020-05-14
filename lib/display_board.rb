bf454289f8ad7a6ae3f4a2cff5683244a94bb03c
Carrie Jones <c.jessica.jones@gmail.com>
2020-05-14 21:46:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board_array)
-  puts " #{board_array[0]} | #{board_array[1]} | 
+  puts " #{board_array[0]} | #{board_array[1]} | #{board_array[2]} "
+  puts "-----------"
+  puts ""
 end
