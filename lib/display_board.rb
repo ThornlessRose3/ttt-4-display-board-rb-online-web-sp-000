ed46d11f8ae3ca141b50b4ce87ff93f55abc566a
Carrie Jones <c.jessica.jones@gmail.com>
2020-05-14 21:46:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,5 +3,7 @@
 def display_board(board_array)
   puts " #{board_array[0]} | #{board_array[1]} | #{board_array[2]} "
   puts "-----------"
-  puts ""
+  puts " #{board_array[3]} | #{board_array[4]} | #{board_array[5]} "
+  puts "-----------"
+  puts " #{board_array[6]} | #{board_array[7]} | #{board_array[8]} "
 end
